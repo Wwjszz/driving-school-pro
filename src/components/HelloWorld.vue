74ce42c18e94a4d79b693316ca1e7ebeb2db89ed
Wwjszz <2301499594@qq.com>
2023-05-31 15:13:06 +0800

    2nd commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,7 +16,7 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p class="text-red-600">
+    <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
